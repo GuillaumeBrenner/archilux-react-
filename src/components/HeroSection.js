005e743fc22ec822fc0eaf2fc8277a068aa25d9a
Guillaume Hounkpati <johndoe@example.com>
2022-04-29 09:19:47 +0000

    contact media query

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      {/* <video src="/videos/video-1.mp4" autoPlay loop muted /> */}
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1 data-aos="fade-right">Where,</h1>
       <p
         data-aos="fade-left"
